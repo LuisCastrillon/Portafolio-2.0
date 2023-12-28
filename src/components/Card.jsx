fb27b45ee17e5e6a40617abc72f65b86533cf441
Luis Castrillon <tutukastrillon@gmail.com>
2023-12-27 22:07:06 -0500

    Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import { cardData } from "../assets/data/cardData";
+import '../css/Card.css';
 
 export const Card = () => {
     // const [cardData, setCardData] = useState();
     
     return(
         <div className="container">
-            <h1>Proyectos</h1>
+            <h1 className="containerT">Proyectos</h1>
             <div className="cardsCnt">
             
             {cardData.map((element, index) => (
@@ -16,7 +17,7 @@ export const Card = () => {
                             <img src={element.img} alt="" />
                         </div>
                         <div className="infCnt">
-                            <h2>{element.title}</h2>
+                            <h1>{element.title}</h1>
                             <p>{element.description}</p>
                             <button>Ver mas...</button>
                         </div>
