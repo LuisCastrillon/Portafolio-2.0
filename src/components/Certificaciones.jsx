33c15d68b4c014f5c4f833149b580ea02e77141f
Luis Castrillon <tutukastrillon@gmail.com>
2024-01-03 19:20:01 -0500

    1.0

--- src/components/Certificaciones.jsx
+++ src/components/Certificaciones.jsx
@@ -5,15 +5,15 @@ import { certificacionesData } from '../assets/data/cardData'
 export const Certificaciones = () => {
     const certificado = (prop) => {
         switch(prop){
-            case 6: window.open('/src/assets/img/platzi.jpg', '_blank');
-            case 4: window.open('/src/assets/img/ingles3.jpg', '_blank')
-            case 3: window.open('/src/assets/img/ingles2.jpg', '_blank');
-            case 2: window.open('/src/assets/img/ingles1.jpg', '_blank');
-            case 1: window.open('/src/assets/img/ingles.jpg', '_blank');
-            case 5: window.open('/src/assets/img/ingles4.jpg', '_blank');
-            case 8: window.open('/src/assets/img/tic.jpg', '_blank');
-            case 0: window.open('/src/assets/img/c.jpg', '_blank');
-            case 7: window.open('/src/assets/img/google.jpg', '_blank');
+            case 6: window.open('/assets/platzi.jpg', '_blank');
+            case 4: window.open('/assets/ingles3.jpg', '_blank')
+            case 3: window.open('/assets/ingles2.jpg', '_blank');
+            case 2: window.open('/assets/ingles1.jpg', '_blank');
+            case 1: window.open('/assets/ingles.jpg', '_blank');
+            case 5: window.open('/assets/ingles4.jpg', '_blank');
+            case 8: window.open('/assets/tic.jpg', '_blank');
+            case 0: window.open('/assets/c.jpg', '_blank');
+            case 7: window.open('/assets/google.jpg', '_blank');
 
 
         }
